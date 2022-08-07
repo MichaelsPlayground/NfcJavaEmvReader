6788f5b1044bd2867ed0d3cf7d4944145e7bd797
michaelfehr <michael_fehrgmx.de>
2022-08-07 22:16:28 +0200

    Initial commit

---
+++ app/src/main/java/de/androidcrypto/nfcjavaemvreader/MainActivity.java
@@ -0,0 +1,308 @@
+package de.androidcrypto.nfcjavaemvreader;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NfcAdapter.ReaderCallback {
+
+    TextView readResult;
+    private NfcAdapter mNfcAdapter;
+    String dumpExportString = "";
+    String tagIdString = "";
+    String tagTypeString = "";
+    private static final int REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE = 100;
+    Context contextSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+        contextSave = getApplicationContext();
+        readResult = findViewById(R.id.tvMainReadResult);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    // This method is run in another thread when a card is discovered
+    // !!!! This method cannot cannot direct interact with the UI Thread
+    // Use `runOnUiThread` method to change the UI from this method
+    @Override
+    public void onTagDiscovered(Tag tag) {
+
+        IsoDep isoDep = null;
+
+        // Whole process is put into a big try-catch trying to catch the transceive's IOException
+        try {
+            isoDep = IsoDep.get(tag);
+            // Make a Sound
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(150, 10));
+            } else {
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(200);
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //UI related things, not important for NFC
+                    readResult.setText("");
+                }
+            });
+            isoDep.connect();
+            byte[] response;
+            String idContentString = "Content of ISO-DEP tag";
+
+
+
+            String finalIdContentString = idContentString;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //UI related things, not important for NFC
+                    readResult.setText(finalIdContentString);
+                }
+            });
+            try {
+                isoDep.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            //Trying to catch any ioexception that may be thrown
+            e.printStackTrace();
+        } catch (Exception e) {
+            //Trying to catch any exception that may be thrown
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public static String prettyPrintCardNumber(String cardNumber) {
+        if (cardNumber == null) return null;
+        char delimiter = ' ';
+        return cardNumber.replaceAll(".{4}(?!$)", "$0" + delimiter);
+    }
+
+    private void showWirelessSettings() {
+        Toast.makeText(this, "You need to enable NFC", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+        startActivity(intent);
+    }
+
+
+    private void writeToUiToast(String message) {
+        runOnUiThread(() -> {
+            Toast.makeText(getApplicationContext(),
+                    message,
+                    Toast.LENGTH_SHORT).show();
+        });
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mNfcAdapter != null) {
+            Bundle options = new Bundle();
+            // Work around for some broken Nfc firmware implementations that poll the card too fast
+            options.putInt(NfcAdapter.EXTRA_READER_PRESENCE_CHECK_DELAY, 250);
+
+            // Enable ReaderMode for all types of card and disable platform sounds
+            // the option NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK is NOT set
+            // to get the data of the tag after reading
+            mNfcAdapter.enableReaderMode(this,
+                    this,
+                    NfcAdapter.FLAG_READER_NFC_A |
+                            NfcAdapter.FLAG_READER_NFC_B |
+                            NfcAdapter.FLAG_READER_NFC_F |
+                            NfcAdapter.FLAG_READER_NFC_V |
+                            NfcAdapter.FLAG_READER_NFC_BARCODE |
+                            NfcAdapter.FLAG_READER_NO_PLATFORM_SOUNDS,
+                    options);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mNfcAdapter != null)
+            mNfcAdapter.disableReaderMode(this);
+    }
+
+    // section for main menu
+
+    private void exportDumpMail() {
+        if (dumpExportString.isEmpty()) {
+            writeToUiToast("Scan a tag first before sending emails :-)");
+            return;
+        }
+        String subject = "Dump NFC-Tag " + tagTypeString + " UID: " + tagIdString;
+        String body = dumpExportString;
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void exportDumpFile() {
+        if (dumpExportString.isEmpty()) {
+            writeToUiToast("Scan a tag first before writing files :-)");
+            return;
+        }
+        verifyPermissionsWriteString();
+    }
+
+    // section external storage permission check
+    private void verifyPermissionsWriteString() {
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[1]) == PackageManager.PERMISSION_GRANTED) {
+            writeStringToExternalSharedStorage();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
+    private void writeStringToExternalSharedStorage() {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        // Optionally, specify a URI for the file that should appear in the
+        // system file picker when it loads.
+        //boolean pickerInitialUri = false;
+        //intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+        // get filename from edittext
+        String filename = tagTypeString + "_" + tagIdString + ".txt";
+        // sanity check
+        if (filename.equals("")) {
+            writeToUiToast("scan a tag before writing the content to a file :-)");
+            return;
+        }
+        intent.putExtra(Intent.EXTRA_TITLE, filename);
+        fileSaverActivityResultLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> fileSaverActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent resultData = result.getData();
+                        // The result data contains a URI for the document or directory that
+                        // the user selected.
+                        Uri uri = null;
+                        if (resultData != null) {
+                            uri = resultData.getData();
+                            // Perform operations on the document using its URI.
+                            try {
+                                // get file content from edittext
+                                String fileContent = dumpExportString;
+                                writeTextToUri(uri, fileContent);
+                                String message = "file written to external shared storage: " + uri.toString();
+                                writeToUiToast("file written to external shared storage: " + uri.toString());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                writeToUiToast("ERROR: " + e.toString());
+                                return;
+                            }
+                        }
+                    }
+                }
+            });
+
+    private void writeTextToUri(Uri uri, String data) throws IOException {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(contextSave.getContentResolver().openOutputStream(uri));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            System.out.println("Exception File write failed: " + e.toString());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+
+        MenuItem mExportMail = menu.findItem(R.id.action_export_mail);
+        mExportMail.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                //Intent i = new Intent(MainActivity.this, AddEntryActivity.class);
+                //startActivity(i);
+                exportDumpMail();
+                return false;
+            }
+        });
+
+        MenuItem mExportFile = menu.findItem(R.id.action_export_file);
+        mExportFile.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                //Intent i = new Intent(MainActivity.this, AddEntryActivity.class);
+                //startActivity(i);
+                exportDumpFile();
+                return false;
+            }
+        });
+
+        MenuItem mClearDump = menu.findItem(R.id.action_clear_dump);
+        mClearDump.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                dumpExportString = "";
+                readResult.setText("read result");
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# NFC Java EMV Reader
+
+
+Build.gradle
+```plaintext
+// https://mvnrepository.com/artifact/commons-cli/commons-cli
+implementation group: 'commons-cli', name: 'commons-cli', version: '1.2'
+// https://mvnrepository.com/artifact/org.jdesktop/appframework
+implementation group: 'org.jdesktop', name: 'appframework', version: '1.0.3'
+// https://mvnrepository.com/artifact/be.cyberelf.nanoxml/lite
+implementation group: 'be.cyberelf.nanoxml', name: 'lite', version: '2.2.3'
+// https://mvnrepository.com/artifact/com.licel/jcardsim
+implementation group: 'com.licel', name: 'jcardsim', version: '2.2.1'
+
+
+
+
+
+
+```
+
+neuere Versionen verfügbar:
+```plaintext
+commons-cli: neue Version 1.5.0 
+// https://mvnrepository.com/artifact/com.licel/jcardsim
+implementation group: 'com.licel', name: 'jcardsim', version: '2.2.2'
+
+
+```
